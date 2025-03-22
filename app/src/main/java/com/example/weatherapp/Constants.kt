f3eba1a42c0b9a13ad28c359965450fd719178fe
demirtasm <m.ddemirtas@hotmail.com>
2025-03-22 14:14:39 +0300

    push

--- app/src/main/java/com/example/weatherapp/Constants.kt
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -17,6 +17,7 @@ object Constants {
             val network = connectivityManager.activeNetwork ?: return false
             val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
             return when {
+                
                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
