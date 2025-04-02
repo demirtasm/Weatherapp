d7530f40a627e89158682ebbfbf08204da1486e4
demirtasm <m.ddemirtas@hotmail.com>
2025-04-02 17:00:00 +0300

    no message

--- app/src/main/java/com/example/weatherapp/adapter/HourlyWeatherAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapter/HourlyWeatherAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.models.HourlyWeather
+import com.example.weatherapp.utils.WeatherCodeUtils
 import kotlin.math.roundToInt
 
 class HourlyWeatherAdapter(private val items: List<HourlyWeather>) :
@@ -32,31 +33,7 @@ class HourlyWeatherAdapter(private val items: List<HourlyWeather>) :
         holder.rainText.text = "%${item.precipitation.toString()}"
         holder.tempText.text = "${item.temperature.roundToInt()}°C"
         holder.weatherCodeImage.setImageResource(
-            when (item.weatherCode) {
-                0 -> R.drawable.ic_clear_day
-                1 -> R.drawable.ic_mainly_clear_day
-                2 -> R.drawable.ic_cloudy_day
-                3 -> R.drawable.ic_overcast_day
-                45, 48 -> R.drawable.ic_haze_day
-                51 -> R.drawable.ic_rain_day
-                53,55 -> R.drawable.ic_dense_rainy_day
-                56 -> R.drawable.ic_clear_day
-                57 -> R.drawable.ic_clear_day
-                61 -> R.drawable.ic_clear_day
-                63 -> R.drawable.ic_clear_day
-                65->R.drawable.ic_clear_day
-                66,67->R.drawable.ic_freezing_rain_day
-                71->R.drawable.ic_clear_day
-                73->R.drawable.ic_clear_day
-                75->R.drawable.ic_clear_day
-                77->R.drawable.ic_clear_day
-                80 -> R.drawable.ic_clear_day
-                81 -> R.drawable.ic_clear_day
-                82 -> R.drawable.ic_clear_day
-                85 -> R.drawable.ic_clear_day
-                86 -> R.drawable.ic_clear_day
-                else -> R.drawable.ic_clear_day
-            }
+            WeatherCodeUtils.getWeatherIconResId(item.weatherCode)
         )
 
     }
