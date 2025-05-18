2042164a65906aff79775345af396107383c2105
demirtasm <m.ddemirtas@hotmail.com>
2025-05-18 16:42:08 +0300

    navigate to splash when the application remains in the background for a long time

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.madkit.weatherapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "APP_ID", "\"${localProperties["APP_ID"]}\"")
     }
--- app/src/main/java/com/madkit/weatherapp/views/activities/MainActivity.kt
+++ app/src/main/java/com/madkit/weatherapp/views/activities/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.madkit.weatherapp.views.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.madkit.weatherapp.R
+import com.madkit.weatherapp.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var lastPausedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onPause() {
+        super.onPause()
+        lastPausedTime = System.currentTimeMillis()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val now = System.currentTimeMillis()
+
+        if (lastPausedTime != 0L && now - lastPausedTime > 3_600_000) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+
+        }
     }
 
 }
